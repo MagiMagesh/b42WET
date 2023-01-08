3135797b93fb76c6f26324ffc56951f41e8e3503
magesh <magesh.engg.65@gmail.com>
2023-01-08 09:34:21 +0530

    second commit

--- newfile.js
+++ newfile.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+// 1. github singin
+// 2. crate a repository
+// 3. to create a new folder in your system
+// 4. inside the terminal - git init 
+// 5. git clone using - git clone https:
+// 6. to go to that File - cd file_name
+// 7. to add the changes to commit - git add .
+// 8. to commit the changes -  git commit -m "first commit"
+// 9. to push the commited changes - git push -u origin main
