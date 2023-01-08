77cd762f15e766ef10d34fc2e0eb718bf2e5764b
magesh <magesh.engg.65@gmail.com>
2023-01-08 09:51:51 +0530

    seventh commit

--- newfile.js
+++ newfile.js
@@ -7,5 +7,7 @@ console.log('hello');
 // 5. git clone using - git clone https:
 // 6. to go to that File - cd file_name
 // 7. to add the changes to commit - git add . [. menas all the flies]
-
+// 8. to commit the changes -  git commit -m "first commit"
+// 9. to push the commited changes - git push -u origin main
+// 10. to get the change happened in github - git pull
 
